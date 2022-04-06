6b70274c991405be538df4e54761e334a8fb3f2b
Debbl <me@aiwan.run>
2022-04-06 22:13:18 +0800

    10_react-redux-初体验

--- 10_react-redux/src/App.jsx
+++ 10_react-redux/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import About from './pages/about';
+import Home from './pages/home';
+import store from './store';
 
 export default function App() {
   return (
     <div>
       <h2>App</h2>
+      <Home />
+      <About />
     </div>
   );
 }
