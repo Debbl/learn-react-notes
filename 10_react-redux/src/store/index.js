ab2ec38ab6246e842da84ece0a47c7e6bed294e1
Debbl <me@aiwan.run>
2022-04-10 13:21:12 +0800

    redux-devtools的使用

--- 10_react-redux/src/store/index.js
+++ 10_react-redux/src/store/index.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import reducer from './reducer';
 
-// 应用中间件
-const storeEnhancer = applyMiddleware(thunkMiddleware);
+// composeEnhances 合并 Enhancers
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true}) || compose;
 
-const store = createStore(reducer, storeEnhancer);
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 export default store;
