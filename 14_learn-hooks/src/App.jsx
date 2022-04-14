02d1158e9a87e4c0a51977afa3fa53841fd49c75
Debbl <me@aiwan.run>
2022-04-14 21:42:15 +0800

    useState()

--- 14_learn-hooks/src/App.jsx
+++ 14_learn-hooks/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import CounterClass from './01_体验hooks/01_counter-class';
+import CounterHook from './01_体验hooks/02_counter-hook';
 
 export default function App() {
   return (
     <div>
-      <h2>App</h2>
+      <h1>1. class组件-计数器</h1>
+      <CounterClass />
+      <h1>2. function 组件 hook - 计数器</h1>
+      <CounterHook />
     </div>
   );
 }
