a7f6550aa5be9171fc23282c989d9dbf7ea874b0
Debbl <me@aiwan.run>
2022-04-15 18:37:04 +0800

    setState()传入一个函数获取前一个值

--- 14_learn-hooks/src/01_体验hooks/02_counter-hook.jsx
+++ 14_learn-hooks/src/01_体验hooks/02_counter-hook.jsx
@@ -3,6 +3,19 @@ import React, { useState } from 'react';
 export default function CounterHook() {
   const [count, setCount] = useState(() => 10);
 
+  function handleClick() {
+    // 只会 + 10
+    // setCount(count + 10);
+    // setCount(count + 10);
+    // setCount(count + 10);
+    // setCount(count + 10);
+
+    // 符合预期
+    setCount((preCount) => preCount + 10);
+    setCount((preCount) => preCount + 10);
+    setCount((preCount) => preCount + 10);
+    setCount((preCount) => preCount + 10);
+  }
   return (
     <div>
       <h2>当前计数: {count}</h2>
@@ -10,6 +23,7 @@ export default function CounterHook() {
       <button onClick={() => setCount((preValue) => preValue + 10)}>
         +10---
       </button>
+      <button onClick={handleClick}>++++</button>
       <button onClick={() => setCount(count - 1)}>-1</button>
     </div>
   );
