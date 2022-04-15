2a54305be4851de208e99ed32b69154b07afd730
Debbl <me@aiwan.run>
2022-04-15 18:33:37 +0800

    useState()传入一个函数

--- 14_learn-hooks/src/01_体验hooks/02_counter-hook.jsx
+++ 14_learn-hooks/src/01_体验hooks/02_counter-hook.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
 export default function CounterHook() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => 10);
+
   return (
     <div>
       <h2>当前计数: {count}</h2>
       <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount((preValue) => preValue + 10)}>
+        +10---
+      </button>
       <button onClick={() => setCount(count - 1)}>-1</button>
     </div>
   );
